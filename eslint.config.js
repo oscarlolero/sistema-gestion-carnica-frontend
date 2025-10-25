f893e39945f0da1ff7a43f33d11fede1d1a870a8
Oscar Montes Camberos <o.montescamberos@gmail.com>
2025-10-25 15:17:02 -0600

    chore: initial setup

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// eslint.config.js (frontend)
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierRecommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+])
